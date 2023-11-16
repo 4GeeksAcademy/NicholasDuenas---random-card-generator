7b2831c53863dba29e239045d05cfca88a441848
NicholasDuenas <145682574+NicholasDuenas@users.noreply.github.com>
2023-11-16 02:27:00 +0000

    "updated .js one final time"

--- src/app.js
+++ src/app.js
@@ -11,20 +11,28 @@ window.onload = function() {
 
   document.querySelector(".numbers").innerHTML = generate_new_number();
   let suit = generate_new_suit();
-  for(let elem of document.querySelectorAll("#bot, #top")){
-    elem.innerHTML = suit
+  console.log(suit);
+  for (let elem of document.querySelectorAll("#bot, #top")) {
+    elem.innerHTML = suit;
+    if (suit == "&diams;" || suit == "&hearts;") {
+      elem.style.color = "red";
+    }
   }
-  // console.log(document.querySelectorAll("#bot, #top"));
+  // if (suit == "&diams;" || suit == "&hearts;"){
+  //     document.querySelector('.suit').style.color = "red";
+  //   }
+  // if suit == "&diams;" {
+  //   document.getElementsByClassName('.suit').Style.color = "red";
+  // }
 };
 
-const suits = ["&hearts;", "&diams;", "&clubs;"];
-const values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+const suits = ["&spades;", "&hearts;", "&diams;", "&clubs;"];
+const values = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
 
 function generate_new_suit() {
   let random_num = Math.floor(Math.random() * suits.length);
   console.log(random_num);
-  let index = suits[random_num];
-  return index;
+  return suits[random_num];
 }
 
 function generate_new_number() {
