55ae582264e2a71244afcd436d0e5043858e0fb0
NicholasDuenas <145682574+NicholasDuenas@users.noreply.github.com>
2023-11-16 01:13:30 +0000

    "updated .js"

--- src/app.js
+++ src/app.js
@@ -8,7 +8,25 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Everything is working");
+
+  document.querySelector(".numbers").innerHTML = generate_new_number();
+  // document.querySelectorAll("#bot, #top").innerHTML = generate_new_suit();
+  // console.log(document.querySelectorAll("#bot, #top"));
 };
-// function generate_random_card = () => {
-// Math.floor(Math.random() * 52);
-// }
+
+const suits = ["&hearts;", "&diams;", "&clubs;"];
+const values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+
+function generate_new_suit() {
+  let random_num = Math.floor(Math.random() * suits.length);
+  console.log(random_num);
+  let index = suits[random_num];
+  return index;
+}
+
+function generate_new_number() {
+  let random_num = Math.floor(Math.random() * values.length);
+  console.log(random_num);
+  console.log(values.toString(values[random_num]));
+  return values[random_num];
+}
